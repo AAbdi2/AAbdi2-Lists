3525250a8a73550661dd333edf2c4cbd6c99d5ab
AAbdi2 <aabdi_2@yahoo.com>
2023-02-09 14:42:43 +0000

    Wk3Day2

--- src/main/java/ListExercise.java
+++ src/main/java/ListExercise.java
@@ -26,11 +26,15 @@ public class ListExercise {
      * the Object form of a primitive.
      *
      * A popular implementation of List is ArrayList. Look up how to instantiate one.
+     * 
      *
      * @return a List<Integer> object.
      */
+
+     List<Integer> list = new ArrayList<Integer>();
+
     public List<Integer> createList(){
-        return null;
+        return list;
     }
 
     /**
@@ -40,7 +44,7 @@ public class ListExercise {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        return list.size();
     }
 
     /**
@@ -52,6 +56,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
+        list.add(value);
     }
 
     /**
@@ -67,7 +72,7 @@ public class ListExercise {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        return list.get(index);
     }
 
     /**
@@ -80,7 +85,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
-
+        list.remove(position);
     }
 
     /**
@@ -93,5 +98,6 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        list.set(position,value);
     }
 }
